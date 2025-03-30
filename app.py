120b1bbb4bd3c06f00e3fbcbaf23e430e7ff4a93
benjamin wright <benjamin.wright.20@cnu.edu>
2025-03-30 19:34:25 -0400

    filter exercise sets to selected exercise, and display them in a simple list

--- app.py
+++ app.py
@@ -18,13 +18,13 @@ class LiftLogGUI:
 
         row1 = ttk.Frame(frm)
         row1.grid(row=1, column=0)
-        ttk.Label(row1, text="Exercise").grid(column=0)
+        ttk.Label(row1, text="Exercise").pack(side=LEFT)
         exercises = sorted(list(self.html_parser.exercises))
-        self.combobox = ttk.Combobox(row1, values=exercises)
-        self.combobox.grid(column=1)
+        self.combobox = ttk.Combobox(row1, values=exercises, width=40)
+        self.combobox.pack(side=RIGHT)
         self.combobox.bind("<<ComboboxSelected>>", self.filter_sets)
 
-        self.text_area = Text(frm, height=24)
+        self.text_area = Text(frm, height=24, width=60)
         self.text_area.grid(row=2, column=0)
 
         ttk.Button(frm, text="Exit", command=root.destroy).grid(row=3, column=0)
@@ -34,9 +34,20 @@ class LiftLogGUI:
             child.grid_configure(padx=5, pady=5)
 
     def filter_sets(self, event: Event):
-        selected_value = self.combobox.get()
+        """
+        Filter the text area content to the exercise selected in the combobox.
+        :param event:  Not used
+        :return:
+        """
+        selected_exercise = self.combobox.get()
         self.text_area.delete("1.0", END)  # Clear existing text
-        self.text_area.insert(END, f"You selected: {selected_value}")  # Update with new text
+
+        to_insert = ""
+        list_sets = self.html_parser.exercise_set_dict[selected_exercise]
+        for the_set in list_sets:
+            to_insert += str(the_set) + "\n"
+
+        self.text_area.insert(END, to_insert)  # Update with new text
 
 
 if __name__ == '__main__':
