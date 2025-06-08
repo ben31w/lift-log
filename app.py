3904393bdce6347029d5597110582853115a872f
benjamin wright <benjamin.wright.20@cnu.edu>
2025-06-07 22:10:46 -0400

    create tabs for various import methods: so far, only HTML tab has been created, and it only checks if an HTML file exists

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 from tkinter import *
 from tkinter import ttk
@@ -93,22 +94,24 @@ class LiftLog(Tk):
         Tk.__init__(self, *args, **kwargs)
         self.title("Lift Log")
         self.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
+        self.minsize(WINDOW_WIDTH, WINDOW_HEIGHT)
 
-        # Init the notebook and tabs
-        self.notebook = ttk.Notebook(self)
-        self.notebook.pack(fill='both', expand=True)
-        tab_my_sets = TabMySets(self.notebook)
+        # Init main notebook and tabs
+        main_notebook = ttk.Notebook(self)
+        main_notebook.pack(fill='both', expand=True)
+        tab_my_sets = TabMySets(main_notebook)
         tab_my_sets.pack(fill='both', expand=True)
-        tab_import_sets = TabImportExercises(self.notebook)
+        tab_import_sets = TabImportSets(main_notebook)
         tab_import_sets.pack(fill='both', expand=True)
-        self.notebook.add(tab_my_sets, text="My Sets")
-        self.notebook.add(tab_import_sets, text="Import Sets")
+        main_notebook.add(tab_my_sets, text="My Sets")
+        main_notebook.add(tab_import_sets, text="Import Sets")
 
 
 class TabMySets(ttk.Frame):
     """
-    A frame where users can view their exercise sets in list and graph form for
-    a selected exercise.
+    This frame is where the user views their exercise sets.
+    The user selects an exercise, and their sets are displayed in text and
+    graphical view.
     """
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent)
@@ -301,8 +304,16 @@ class TabMySets(ttk.Frame):
         canvas.get_tk_widget().grid(row=plot_grid_row, column=plot_grid_col)
 
 
-class TabImportExercises(ttk.Frame):
-    """A frame that allows the user to import exercise sets via HTML or Apple Notes."""
+class TabImportSets(ttk.Frame):
+    """
+    This frame is where the user imports exercise sets from various sources.
+    - HTML imports (work-in-progress)
+    - Apple Notes (TODO)
+    - Manual imports (TODO)
+    - Picture of journal (very ambitious TODO).
+
+    There is an import status window and a way to remove previous imports (TODO actually implement).
+    """
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent)
 
@@ -315,24 +326,11 @@ class TabImportExercises(ttk.Frame):
         # Container row 1
         row1 = ttk.Frame(self)
         row1.grid(row=1, column=0, sticky=W)
-
-        frm_html = ttk.Frame(row1)
-        frm_html.grid(row=0, column=0)
-        lbl_via_html = ttk.Label(frm_html, text="Via HTML File")
-        lbl_via_html.grid(row=0, column=0)
-        lbl_via_html_desc = ttk.Label(frm_html, text="You can import exercise sets via an existing HTML file.")
-        lbl_via_html_desc.grid(row=1, column=0)
-        btn_browse = ttk.Button(frm_html, text="Browse")
-        btn_browse.grid(row=2, column=0)
-
-        frm_apple = ttk.Frame(row1)
-        frm_apple.grid(row=0, column=1)
-        lbl_via_apple = ttk.Label(frm_apple, text="Via Apple Notes")
-        lbl_via_apple.grid(row=0, column=0)
-        lbl_via_apple_desc = ttk.Label(frm_apple, text="You can import exercise sets via Apple Notes.")
-        lbl_via_apple_desc.grid(row=1, column=0)
-        btn_go = ttk.Button(frm_apple, text="Go")
-        btn_go.grid(row=2, column=0)
+        import_method_notebook = ttk.Notebook(row1)
+        import_method_notebook.grid(row=0, column=0)
+        tab_import_via_html = SubTabImportSetsViaHTML(import_method_notebook)
+        tab_import_via_html.pack(fill='both', expand=True)
+        import_method_notebook.add(tab_import_via_html, text="HTML")
 
         # Container row 2
         row2 = ttk.Frame(self)
@@ -397,12 +395,75 @@ class TabImportExercises(ttk.Frame):
 
         pad_frame(self)
         pad_frame(row1)
-        pad_frame(frm_html)
-        pad_frame(frm_apple)
         pad_frame(imports_table)
 
     # def build_imports_table(self):
 
+
+class SubTabImportSetsViaHTML(ttk.Frame):
+    """
+    This frame is where the user imports sets via an HTML file.
+    """
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent)
+
+        # Container row 0
+        row0 = ttk.Frame(self)
+        row0.grid(row=0, column=0, sticky=W)
+        lbl_import_via_html = ttk.Label(row0, text="Import sets with an HTML and alias (TXT) file.")
+        lbl_import_via_html.grid(row=0, column=0)
+
+        # Container row 1
+        row1 = ttk.Frame(self)
+        row1.grid(row=1, column=0, sticky=W)
+        lbl_html_filepath = ttk.Label(row1, text="HTML Filepath")
+        lbl_html_filepath.grid(row=0, column=0)
+        self.entry_html_filepath = ttk.Entry(row1, width=50)
+        self.entry_html_filepath.grid(row=0, column=1)
+        btn_browse_html = ttk.Button(row1, text="Browse")
+        btn_browse_html.grid(row=0, column=2)
+
+        # Container row 2
+        row2 = ttk.Frame(self)
+        row2.grid(row=2, column=0, sticky=W)
+        lbl_alias_filepath = ttk.Label(row2, text="Alias Filepath")
+        lbl_alias_filepath.grid(row=0, column=0)
+        entry_alias_filepath = ttk.Entry(row2, width=50)
+        entry_alias_filepath.grid(row=0, column=1)
+        btn_browse_alias = ttk.Button(row2, text="Browse")
+        btn_browse_alias.grid(row=0, column=2)
+
+        # Container row 3
+        row3 = ttk.Frame(self)
+        row3.grid(row=3, column=0, sticky=W)
+        btn_import_html = ttk.Button(row3, text="Import", command=self.import_html_file)
+        btn_import_html.grid(row=0, column=0)
+        self.error_msg = StringVar()
+        self.lbl_error_msg = ttk.Label(row3, text="", foreground="red")  # Empty error msg by default
+        self.lbl_error_msg.grid(row=0, column=1)
+
+        pad_frame(self)
+
+    def browse_html_file(self):
+        """Open window to browse for an HTML file."""
+        # TODO
+
+    def browse_alias_file(self):
+        """Open window to browse for an alias (TXT) file."""
+        # TODO
+
+    def import_html_file(self):
+        """Import the HTML file that the user has selected."""
+        print("import_html_file")
+        html_file = self.entry_html_filepath.get()
+        print(f"HTML FILE: {html_file}")
+        if not os.path.exists(html_file):
+            self.lbl_error_msg.config(text=f"HTML file '{html_file}' does not exist.")
+        elif len(html_file) > 5 and html_file[-5:] != '.html':
+            self.lbl_error_msg.config(text=f"'{html_file}' is not an HTML file.")
+        else:
+            self.lbl_error_msg.config(text=f"HTML file '{html_file}' exists.")
+            # TODO process HTML file
 
 if __name__ == '__main__':
     lift_log = LiftLog()
